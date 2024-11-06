197c397bd7f297550079d124c3240528901ed1c1
Carita Lindfors <caritalindf@gmail.com>
2024-11-06 14:35:03 +0200

    App.jsx

---
+++ osa2/2b/2.6-2.10/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  // Handler for form submission
+  const addPerson = (event) => {
+    event.preventDefault() // Prevent page reload
+    const personObject = { name: newName }
+
+    setPersons(persons.concat(personObject)) // Add new person to the list
+    setNewName('') // Clear the input field after submission
+  }
+
+  // Handler for input change
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <div>debug: {newName}</div> {/* For debugging, remove when not needed */}
+
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => 
+          <li key={person.name}>{person.name}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
